0e9ab36a0b1558d9803541789001c35cca2bd0c7
alemlodyigor <szeregowygpim@gmail.com>
2023-09-05 16:31:44 +0200

    added footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import classess from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={classess.footer}>
+      <p>
+        &copy; alemlodyigor Do not copy code or any part of that without
+        permissions
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
