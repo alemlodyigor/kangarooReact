e70ebdb9defd278af620250c462a8ed227de6eb3
alemlodyigor <szeregowygpim@gmail.com>
2023-09-07 23:45:07 +0200

    added insert query

--- src/components/functions/insert.js
+++ src/components/functions/insert.js
@@ -1,3 +1,16 @@
-export const insert = props => {
-    
+export const insert = (data) => {
+    if (!Array.isArray(data) || data.length === 0) return;
+
+    const columns = Object.keys(data[0]);
+
+    const insertValues = data.map(row => {
+        const values = columns.map(column => {
+            const value = row[column];
+            return typeof value === 'string ' ? `'${value}'` : value;
+        })
+        return `(${values.join(', ')})`;
+    }).join(',\n');
+
+    const query = `INSERT INTO ${data.tableName} (${columns.join(', ')}) VALUES\n${insertValues}`;
+    return query;
 }
