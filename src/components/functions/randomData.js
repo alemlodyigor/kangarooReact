7d3b671e99ecdb8f4084ca2fcc5cd4a4ce0776c0
alemlodyigor <szeregowygpim@gmail.com>
2023-09-07 23:31:45 +0200

    added random birth date

--- src/components/functions/randomData.js
+++ src/components/functions/randomData.js
@@ -6,3 +6,14 @@ export const generateCreditCardNumber = () => {
     return cardNumber;
 }
 
+export const generateBirthDate = () => {
+    const startYear = 1923;
+    const endYear = 2023;
+    const birthYear = faker.random.number({min: startYear, max: endYear});
+    const birthMonth = faker.random.number({min: 1, max: 12});
+    const birthDay = faker.random.number({min: 1, max: 28});
+
+    const birthDate = new Date(birthYear, birthMonth -1, birthDay);
+    
+    return birthDate.toLocaleDateString();
+}
